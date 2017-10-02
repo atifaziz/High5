cb82c13a5db365ec99b0bc4823948661ffd3e0fb
Atif Aziz <code@raboof.com>
2017-10-02 08:10:55 +0200

    Put Attr into a namespace

--- src/Attr.cs
+++ src/Attr.cs
@@ -22,18 +22,21 @@
 //
 #endregion
 
-sealed class Attr
+namespace ParseFive
 {
-    public string Prefix { get; internal set; }
-    public string Name { get; internal set; }
-    public string Namespace { get; internal set; }
-    public string Value { get; internal set; }
-
-    public Attr(string name, string value)
+    sealed class Attr
     {
-        this.Prefix = "";
-        this.Name = name;
-        this.Namespace = "";
-        this.Value = value;
+        public string Prefix { get; internal set; }
+        public string Name { get; internal set; }
+        public string Namespace { get; internal set; }
+        public string Value { get; internal set; }
+
+        public Attr(string name, string value)
+        {
+            this.Prefix = "";
+            this.Name = name;
+            this.Namespace = "";
+            this.Value = value;
+        }
     }
 }
