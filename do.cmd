8039456283a1a4fc444ad55a7f013cff9c6fe3b4
Atif Aziz <code@raboof.com>
2017-10-12 08:28:13 +0200

    Scripts for building & packaging

---
+++ do.cmd
@@ -0,0 +1,41 @@
+@echo off
+pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
+setlocal
+where dotnet >nul 2>&1 || (
+    echo>&2 .NET Core does not appear to be installed on this machine, which is
+    echo>&2 required to build the solution. You can install it from the URL below
+    echo>&2 and then try building again:
+    echo>&2 https://dot.net
+    exit 1
+)
+dotnet --info && call :%1 %2 %3 %4 %5 %6 %7 %8 %9
+goto :EOF
+
+:build
+    dotnet build -c Debug ^
+ && dotnet build -c Release
+goto :EOF
+
+:test
+    call :build ^
+ && dotnet test --no-restore --no-build -c Debug   tests\Fixtures ^
+ && dotnet test --no-restore --no-build -c Release tests\Fixtures
+goto :EOF
+
+:pack
+setlocal
+set VERSION_SUFFIX=
+if not "%~1"=="" set VERSION_SUFFIX=--version-suffix %1
+call :build ^
+  && dotnet pack -c Release -o ..\dist ^
+                 --no-restore          ^
+                 --no-build            ^
+                 --include-symbols     ^
+                 --include-source      ^
+                 %VERSION_SUFFIX%
+goto :EOF
--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[[ -e test.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
+[[ -e build.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 which dotnet 2>/dev/null || {
     echo>&2 .NET Core does not appear to be installed on this machine, which is
     echo>&2 required to build the solution. You can install it from the URL below
@@ -9,6 +9,4 @@ which dotnet 2>/dev/null || {
 }
 set -e
 dotnet --info
-for c in Debug Release; do
-    dotnet test -c $c tests/Fixtures
-done
+dotnet pack -c Release -o ../dist src
