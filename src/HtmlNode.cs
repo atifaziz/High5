d068f6beed31b584019200614833c37688bc0a08
Atif Aziz <code@raboof.com>
2017-11-15 09:56:25 +0000

    Simplification of the red tree model

--- src/HtmlNode.cs
+++ src/HtmlNode.cs
@@ -42,12 +42,12 @@ namespace High5
         internal void RemoveChildNodeAt(int index) => _childNodes.RemoveAt(index);
     }
 
-    public sealed class HtmlDocument : HtmlNode, IHtmlTreeRootable
+    public sealed class HtmlDocument : HtmlNode
     {
         public string Mode { get; internal set; }
     }
 
-    public sealed class HtmlDocumentFragment : HtmlNode, IHtmlTreeRootable {}
+    public sealed class HtmlDocumentFragment : HtmlNode {}
 
     public sealed class HtmlDocumentType : HtmlNode
     {
@@ -119,7 +119,7 @@ namespace High5
         }
     }
 
-    public class HtmlElement : HtmlNode, IHtmlTreeRootable
+    public class HtmlElement : HtmlNode
     {
         ReadOnlyCollection<HtmlAttribute> _attrs = new ReadOnlyCollection<HtmlAttribute>();
 
--- src/HtmlTree.cs
+++ src/HtmlTree.cs
@@ -25,294 +25,149 @@
 namespace High5
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using Microsoft.Extensions.Internal;
 
-    public interface IHtmlTree
+    public static class ListNode
     {
-        IHtmlTreeNode Root { get; }
-        IHtmlTreeNode GetParent(HtmlNode node);
+        public static ListNode<T> Create<T>(T item) => new ListNode<T>(item, null);
     }
 
-    public interface IHtmlTreeRootable { }
-
-    public interface IHtmlTree<T> : IHtmlTree
-        where T : HtmlNode, IHtmlTreeRootable
+    public class ListNode<T> : IReadOnlyCollection<T>, IEquatable<ListNode<T>>
     {
-        new HtmlTreeNode<T, T> Root { get; }
-        bool TryGetParent(HtmlNode node, out HtmlTreeNode<T, HtmlNode> parent);
-    }
+        public static readonly ListNode<T> Empty = new ListNode<T>();
 
-    public interface IHtmlTreeNode
-    {
-        IHtmlTree Tree { get; }
-        HtmlNode Node { get; }
-    }
+        public int Count { get; internal set; }
+        public bool IsEmpty => Next == null;
+        public ListNode<T> Next { get; }
+        public T Item { get; }
 
-    public interface IHtmlTreeNode<TRoot, out TNode> : IHtmlTreeNode
-        where TRoot : HtmlNode, IHtmlTreeRootable
-        where TNode : HtmlNode
-    {
-        new IHtmlTree<TRoot> Tree { get; }
-        new TNode Node { get; }
-    }
+        public ListNode() { }
 
-    public static class HtmlTreeNode
-    {
-        public static HtmlTreeNode<TRoot, TNode> Create<TRoot, TNode>(IHtmlTree<TRoot> root, TNode node)
-            where TRoot : HtmlNode, IHtmlTreeRootable
-            where TNode : HtmlNode =>
-            new HtmlTreeNode<TRoot, TNode>(root, node);
-    }
-
-    public struct HtmlTreeNode<TRoot, TNode> :
-        IHtmlTreeNode<TRoot, TNode>,
-        IEquatable<HtmlTreeNode<TRoot, TNode>>
-        where TRoot : HtmlNode, IHtmlTreeRootable
-        where TNode : HtmlNode
-    {
-        public HtmlTreeNode(IHtmlTree<TRoot> tree, TNode node)
+        public ListNode(T item, ListNode<T> next)
         {
-            Tree = tree ?? throw new ArgumentNullException(nameof(tree));
-            Node = node ?? throw new ArgumentNullException(nameof(node));
+            Next = next;
+            Item = item;
+            Count = Next.Count + 1;
         }
 
-        public IHtmlTree<TRoot> Tree { get; }
+        public ListNode<T> Prepend(T item) => new ListNode<T>(item, this);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var node = this; node != null; node = node.Next)
+                yield return node.Item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool Equals(ListNode<T> other) =>
+            ReferenceEquals(this, other)
+            || Count == other.Count
+            && EqualityComparer<T>.Default.Equals(Item, other.Item)
+            && (ReferenceEquals(Next, other.Next) || Equals(Next, other.Next));
+
+        public override bool Equals(object obj) =>
+            obj is ListNode<T> list && Equals(list);
+
+        public override int GetHashCode()
+        {
+            var hash = HashCodeCombiner.Start();
+            hash.Add(EqualityComparer<T>.Default.GetHashCode(Item));
+            hash.Add((object) Next);
+            return hash.CombinedHash;
+        }
+
+        public static bool operator ==(ListNode<T> left, ListNode<T> right) =>
+            Equals(left, right);
+
+        public static bool operator !=(ListNode<T> left, ListNode<T> right) =>
+            !Equals(left, right);
+    }
+
+    static class HtmlTreeNode
+    {
+        public static HtmlTreeNode<TNode> Create<TNode>(TNode node, ListNode<HtmlNode> ancestors)
+            where TNode : HtmlNode =>
+            new HtmlTreeNode<TNode>(node, ancestors);
+    }
+
+    public struct HtmlTreeNode<TNode> : IEquatable<HtmlTreeNode<TNode>>
+        where TNode : HtmlNode
+    {
+        readonly ListNode<HtmlNode> _ancestors;
+
+        internal HtmlTreeNode(TNode node, ListNode<HtmlNode> ancestors)
+        {
+            Node = node ?? throw new ArgumentNullException(nameof(node));
+            _ancestors = ancestors ?? throw new ArgumentNullException(nameof(ancestors));
+        }
+
+        public bool HasParent => _ancestors?.IsEmpty == false;
+
+        public HtmlTreeNode<HtmlNode> Parent =>
+            HasValue
+            ? HasParent
+              ? HtmlTreeNode.Create(_ancestors.Item, _ancestors.Next)
+              : default(HtmlTreeNode<HtmlNode>)
+            : throw new InvalidOperationException();
+
         public TNode Node { get; }
 
-        public bool HasValue => Tree == null;
+        public bool HasValue => Node != null;
 
-        public HtmlTreeNode<TRoot, HtmlNode> AsBaseNode() => Tree.WithNode((HtmlNode) Node);
+        public HtmlTreeNode<HtmlNode> AsBaseNode() => HtmlTreeNode.Create((HtmlNode) Node, _ancestors);
 
         public int ChildNodeCount => Node.ChildNodes.Count;
-        public bool HasChildNode  => ChildNodeCount > 0;
+        public bool HasChildNodes => ChildNodeCount > 0;
 
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> ChildNodes
+        public IEnumerable<HtmlTreeNode<HtmlNode>> ChildNodes
         {
             get
             {
-                var tree = Tree;
-                return tree != null
-                     ? from child in Node.ChildNodes
-                       select tree.WithNode(child)
-                     : Enumerable.Empty<HtmlTreeNode<TRoot, HtmlNode>>();
+                var parent = Node;
+                var ancestors = _ancestors.Prepend(parent);
+                return from child in parent.ChildNodes
+                       select HtmlTreeNode.Create(child, ancestors);
             }
         }
 
-        public HtmlTreeNode<TRoot, HtmlNode> Parent =>
-            Tree.TryGetParent(Node, out var parent)
-            ? parent
-            : default(HtmlTreeNode<TRoot, HtmlNode>);
-
-        public HtmlTreeNode<TRoot, HtmlNode> PreviousSibling => GetSibling(-1, (i, _) => i >= 0);
-        public HtmlTreeNode<TRoot, HtmlNode> NextSibling => GetSibling(+1, (i, count) => i < count);
-
-        HtmlTreeNode<TRoot, HtmlNode> GetSibling(int offset, Func<int, int, bool> predicate)
-        {
-            var siblings = Parent.Node.ChildNodes;
-            var index = -1;
-            for (var i = 0; i < siblings.Count; i++)
-            {
-                var sibling = siblings[i];
-                if (sibling == Node)
-                {
-                    index = i;
-                    break;
-                }
-            }
-            return index >= 0  && predicate(index + offset, siblings.Count)
-                 ? Tree.WithNode(siblings[index + offset])
-                 : default(HtmlTreeNode<TRoot, HtmlNode>);
-        }
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> NodesBeforeSelf()
-        {
-            var node = AsBaseNode();
-            while ((node = node.PreviousSibling).HasValue)
-                yield return node;
-        }
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> NodesAfterSelf()
-        {
-            var node = Tree.WithNode((HtmlNode) Node);
-            while ((node = node.NextSibling).HasValue)
-                yield return node;
-        }
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> Descendants()
-        {
-            foreach (var child in ChildNodes)
-            {
-                yield return child;
-                foreach (var descendant in child.Descendants())
-                    yield return descendant;
-            }
-        }
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> DescendantsAndSelf() =>
-            Enumerable.Repeat(AsBaseNode(), 1).Concat(Descendants());
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> Ancestors()
-        {
-            for (var node = Parent; node.HasValue; node = node.Parent)
-                yield return node;
-        }
-
-        public IEnumerable<HtmlTreeNode<TRoot, HtmlNode>> AncestorsAndSelf() =>
-            Enumerable.Repeat(AsBaseNode(), 1).Concat(Ancestors());
-
-        IHtmlTree IHtmlTreeNode.Tree => Tree;
-        HtmlNode  IHtmlTreeNode.Node => Node;
-
-        public bool Equals(HtmlTreeNode<TRoot, TNode> other) =>
-            Tree == other.Tree && Node == other.Node;
+        public bool Equals(HtmlTreeNode<TNode> other) =>
+            (ReferenceEquals(_ancestors, other._ancestors) || _ancestors == other._ancestors)
+            && Node == other.Node;
 
         public override bool Equals(object obj) =>
-            obj is HtmlTreeNode<TRoot, TNode> node && Equals(node);
+            obj is HtmlTreeNode<TNode> node && Equals(node);
 
-        public override int GetHashCode() =>
-            unchecked((Tree?.GetHashCode() ?? 0) * 397) ^ (Node?.GetHashCode() ?? 0);
+        public override int GetHashCode()
+        {
+            var hash = HashCodeCombiner.Start();
+            hash.Add(_ancestors?.GetHashCode());
+            hash.Add(Node);
+            return hash.CombinedHash;
+        }
 
-        public static bool operator ==(HtmlTreeNode<TRoot, TNode> left, HtmlTreeNode<TRoot, TNode> right) =>
+        public static bool operator ==(HtmlTreeNode<TNode> left, HtmlTreeNode<TNode> right) =>
             left.Equals(right);
 
-        public static bool operator !=(HtmlTreeNode<TRoot, TNode> left, HtmlTreeNode<TRoot, TNode> right) =>
+        public static bool operator !=(HtmlTreeNode<TNode> left, HtmlTreeNode<TNode> right) =>
             !left.Equals(right);
     }
 
     public static class HtmlTree
     {
-        public static IHtmlTree<HtmlDocument> Create(HtmlDocument document) =>
-            Tree<HtmlDocument>.Create(document);
+        public static HtmlTreeNode<HtmlDocument> Create(HtmlDocument document) =>
+            TreeFromNode(document);
 
-        public static IHtmlTree<HtmlDocumentFragment> Create(HtmlDocumentFragment documentFragment) =>
-            Tree<HtmlDocumentFragment>.Create(documentFragment);
+        public static HtmlTreeNode<HtmlDocumentFragment> Create(HtmlDocumentFragment documentFragment) =>
+            TreeFromNode(documentFragment);
 
-        public static IHtmlTree<HtmlElement> Create(HtmlElement element) =>
-            Tree<HtmlElement>.Create(element);
+        public static HtmlTreeNode<HtmlElement> Create(HtmlElement element) =>
+            TreeFromNode(element);
 
-        sealed class Tree<T> : IHtmlTree<T> where T : HtmlNode, IHtmlTreeRootable
-        {
-            readonly Dictionary<HtmlNode, HtmlNode> _parentByNode;
-
-            Tree(T root, Dictionary<HtmlNode, HtmlNode> parentByNode)
-            {
-                Root = this.WithNode(root);
-                _parentByNode = parentByNode;
-            }
-
-            public HtmlTreeNode<T, T> Root { get; }
-
-            IHtmlTreeNode IHtmlTree.Root => Root;
-
-            IHtmlTreeNode IHtmlTree.GetParent(HtmlNode node) =>
-                TryGetParent(node, out var parent) ? (IHtmlTreeNode) parent : null;
-
-            public bool TryGetParent(HtmlNode node, out HtmlTreeNode<T, HtmlNode> parent)
-            {
-                if (node == null) throw new ArgumentNullException(nameof(node));
-
-                if (node == Root.Node)
-                {
-                    parent = default(HtmlTreeNode<T, HtmlNode>);
-                    return false;
-                }
-
-                if (!_parentByNode.TryGetValue(node, out var parentNode))
-                    throw new ArgumentException("Node does not belong to this tree.");
-
-                parent = this.WithNode(parentNode);
-                return true;
-                /*
-                return node != Root.Node
-                     ? _parentByNode.TryGetValue(node, out var parentNode)
-                       ? new HtmlTreeNode<T, HtmlNode>(this, parentNode)
-                       : throw new ArgumentException("Node does not belong to this tree.")
-                     : default(HtmlTreeNode<T, HtmlNode>);
-                     */
-            }
-
-            internal static Tree<T> Create(T root)
-            {
-                var parentByNode = new Dictionary<HtmlNode, HtmlNode>();
-                Walk(root);
-
-                void Walk(HtmlNode parent)
-                {
-                    foreach (var child in parent.ChildNodes)
-                    {
-                        parentByNode.Add(child, parent);
-                        Walk(child);
-                    }
-                }
-
-                return new Tree<T>(root, parentByNode);
-            }
-        }
-    }
-
-    public static class HtmlTreeExtensions
-    {
-        public static HtmlTreeNode<TRoot, TNode> WithNode<TRoot, TNode>(this IHtmlTree<TRoot> tree, TNode node)
-            where TRoot : HtmlNode, IHtmlTreeRootable
-            where TNode : HtmlNode =>
-            HtmlTreeNode.Create(tree, node);
-
-        public static IEnumerable<HtmlTreeNode<TRoot, HtmlElement>> Elements<TRoot, TNode>(this IEnumerable<HtmlTreeNode<TRoot, TNode>> nodes)
-            where TRoot : HtmlNode, IHtmlTreeRootable
-            where TNode : HtmlNode =>
-            from node in nodes.Where(e => e.Node is HtmlElement) select node.Tree.WithNode((HtmlElement) (object) node.Node);
-        /*
-        public static IEnumerable<HtmlElement> Elements(this HtmlNode node) =>
-            node?.ChildNodes.Elements()
-            ?? throw new ArgumentNullException(nameof(node));
-
-        public static IEnumerable<HtmlElement> ElementsAfterSelf(this HtmlNode node) =>
-            node?.NodesAfterSelf().Elements()
-            ?? throw new ArgumentNullException(nameof(node));
-
-        public static IEnumerable<HtmlElement> ElementsBeforeSelf(this HtmlNode node) =>
-            node?.NodesBeforeSelf().Elements()
-            ?? throw new ArgumentNullException(nameof(node));
-            */
-        /*
-        public static HtmlNode GetPreviousSibling(this IHtmlTree tree, HtmlNode node) =>
-            GetSibling(tree, node, -1, (i, _) => i >= 0);
-        public static HtmlNode GetNextSibling(this IHtmlTree tree, HtmlNode node) =>
-            GetSibling(tree, node, +1, (i, count) => i < count);
-
-        public static HtmlNode GetSibling(this IHtmlTree tree, HtmlNode node, int offset, Func<int, int, bool> predicate)
-        {
-            if (tree == null) throw new ArgumentNullException(nameof(tree));
-            if (node == null) throw new ArgumentNullException(nameof(node));
-
-            var siblings = tree.GetParent(node)?.ChildNodes;
-            var index = siblings?.IndexOf(node);
-            return index is int i && predicate(i + offset, siblings.Count)
-                ? siblings[i + offset]
-                : null;
-        }
-
-        public static IEnumerable<HtmlNode> NodesBeforeSelf(this IHtmlTree tree, HtmlNode node)
-        {
-            while ((node = tree.GetPreviousSibling(node)) != null)
-                yield return node;
-        }
-
-        public static IEnumerable<HtmlNode> NodesAfterSelf(this IHtmlTree tree, HtmlNode node)
-        {
-            while ((node = tree.GetNextSibling(node)) != null)
-                yield return node;
-        }
-
-        public static IEnumerable<HtmlNode> Ancestors(this IHtmlTree tree, HtmlNode node)
-        {
-            for (var parent = tree.GetParent(node); parent != null; parent = parent.ParentNode)
-                yield return parent;
-        }
-
-        public static IEnumerable<HtmlNode> AncestorsAndSelf(this IHtmlTree tree, HtmlNode node) =>
-            Enumerable.Repeat(node, 1).Concat(tree.Ancestors(node));
-        */
+        static HtmlTreeNode<T> TreeFromNode<T>(T root) where T : HtmlNode =>
+            HtmlTreeNode.Create(root, ListNode<HtmlNode>.Empty);
     }
 }
