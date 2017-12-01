77389964c9f78bd73043a6d5a79d3c0a8269a427
Atif Aziz <code@raboof.com>
2017-12-01 11:15:41 +0100

    Reorg HtmlNode tests using nested class per factory

---
+++ tests/Fixtures/HtmlNodeFactoryTests.cs
@@ -0,0 +1,379 @@
+#region Copyright (c) 2017 Atif Aziz
+//
+// Portions Copyright (c) 2013 Ivan Nikulin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
+
+namespace High5.Tests
+{
+    using System.Linq;
+    using Xunit;
+    using static HtmlNodeFactory;
+
+    public class HtmlNodeFactoryTests
+    {
+        public class Text
+        {
+            [Fact]
+            public void ReturnsTextNodeInitialzed()
+            {
+                const string value = "foobar";
+                var text = Text(value);
+                Assert.NotNull(text);
+                Assert.Equal(value, text.Value);
+                Assert.Equal(0, text.ChildNodes.Count);
+            }
+        }
+
+        public class Comment
+        {
+            [Fact]
+            public void ReturnsCommentNodeInitialzed()
+            {
+                const string data = "some comment";
+                var comment = Comment(data);
+                Assert.NotNull(comment);
+                Assert.Equal(data, comment.Data);
+                Assert.Equal(0, comment.ChildNodes.Count);
+            }
+        }
+
+        public class Attribute
+        {
+            internal static void AssertEqual(string expectedName,
+                                             string expectedValue,
+                                             HtmlAttribute actualAttribute)
+            {
+                Assert.NotNull(actualAttribute);
+                Assert.Equal(expectedName, actualAttribute.Name);
+                Assert.Equal(expectedValue, actualAttribute.Value);
+                Assert.Equal(null, actualAttribute.NamespaceUri);
+                Assert.Equal(null, actualAttribute.Prefix);
+            }
+
+            [Fact]
+            public void ReturnsAttributeInitialized()
+            {
+                const string name = "name";
+                const string value = "value";
+
+                AssertEqual(name, value, Attribute(name, value));
+            }
+
+            [Fact]
+            public void Deconstruction()
+            {
+                const string name = "name";
+                const string value = "value";
+
+                var attribute = Attribute(name, value);
+                var (n, v) = attribute;
+                Assert.Equal(n, name);
+                Assert.Equal(v, value);
+            }
+
+            [Fact]
+            public void DeconstructionWithNamespace()
+            {
+                const string name = "name";
+                const string value = "value";
+
+                var attribute = Attribute(name, value);
+                var (ns, n, v) = attribute;
+                Assert.Equal(ns, null);
+                Assert.Equal(n, name);
+                Assert.Equal(v, value);
+            }
+
+            [Fact]
+            public void DeconstructionWithNamespaceAndPrefix()
+            {
+                const string name = "name";
+                const string value = "value";
+
+                var attribute = Attribute(name, value);
+                var (ns, prefix, n, v) = attribute;
+                Assert.Equal(ns, null);
+                Assert.Equal(prefix, null);
+                Assert.Equal(n, name);
+                Assert.Equal(v, value);
+            }
+        }
+
+        public class Attributes
+        {
+            [Fact]
+            public void AttributesReturnsSequenceOfInitializedAttributes()
+            {
+                const string name1  = "name1";
+                const string value1 = "value2";
+                const string name2  = "name1";
+                const string value2 = "value2";
+
+                using (var e = Attributes((name1, value1), (name2, value2)).GetEnumerator())
+                {
+                    Assert.True(e.MoveNext());
+                    Attribute.AssertEqual(name1, value1, e.Current);
+
+                    Assert.True(e.MoveNext());
+                    Attribute.AssertEqual(name2, value2, e.Current);
+
+                    Assert.False(e.MoveNext());
+                }
+            }
+        }
+
+        public class Element
+        {
+            const string HtmlNs = "http://www.w3.org/1999/xhtml";
+
+            [Fact]
+            public void ReturnsElementNodeInitialzed()
+            {
+                const string tagName = "element";
+                var element = Element(tagName);
+
+                Assert.NotNull(element);
+                Assert.Equal(tagName, element.TagName);
+                Assert.Equal(HtmlNs, element.NamespaceUri);
+                Assert.Equal(0, element.Attributes.Count);
+                Assert.Equal(0, element.ChildNodes.Count);
+            }
+
+            [Fact]
+            public void WithAttributesReturnsElementNodeInitialzedSuch()
+            {
+                const string tagName = "element";
+                const string name1 = "name1"; const string value1 = "value1";
+                const string name2 = "name2"; const string value2 = "value2";
+
+                var element =
+                    Element(tagName,
+                        Attribute(name1, value1),
+                        Attribute(name2, value2));
+
+                Assert.NotNull(element);
+                Assert.Equal(tagName, element.TagName);
+                Assert.Equal(HtmlNs, element.NamespaceUri);
+
+                Assert.Equal(2, element.Attributes.Count);
+                Attribute.AssertEqual(name1, value1, element.Attributes[0]);
+                Attribute.AssertEqual(name2, value2, element.Attributes[1]);
+
+                Assert.Equal(0, element.ChildNodes.Count);
+            }
+
+            [Fact]
+            public void WithAttributesAndChildrenReturnsElementNodeInitialzedSuch()
+            {
+                const string tagName = "element";
+                const string name1  = "name1";
+                const string value1 = "value2";
+                const string name2  = "name1";
+                const string value2 = "value2";
+
+                var child1 = Text("text");
+                var child2 = Comment("some comment");
+                var child3 = Element("child");
+
+                var element =
+                    Element(tagName,
+                        Attributes((name1, value1), (name2, value2)),
+                        child1,
+                        child2,
+                        child3);
+
+                Assert.NotNull(element);
+                Assert.Equal(tagName, element.TagName);
+                Assert.Equal(HtmlNs, element.NamespaceUri);
+
+                Assert.Equal(2, element.Attributes.Count);
+                Attribute.AssertEqual(name1, value1, element.Attributes[0]);
+                Attribute.AssertEqual(name2, value2, element.Attributes[1]);
+
+                Assert.Equal(3, element.ChildNodes.Count);
+                Assert.Same(child1, element.ChildNodes[0]);
+                Assert.Same(child2, element.ChildNodes[1]);
+                Assert.Same(child3, element.ChildNodes[2]);
+            }
+
+            [Fact]
+            public void WithStringReturnsElementWithStringAsTextNode()
+            {
+                const string tagName = "p";
+                const string text = "This is a paragraph.";
+                var e = Element(tagName, text);
+
+                Assert.NotNull(e);
+                Assert.Equal(tagName, e.TagName);
+
+                Assert.Empty(e.Attributes);
+
+                Assert.Equal(1, e.ChildNodes.Count);
+                Assert.IsType<HtmlText>(e.FirstChild);
+                Assert.Equal(text, ((HtmlText) e.FirstChild).Value);
+            }
+
+            [Fact]
+            public void WithAttributesAndStringReturnsElementWithStringAsTextNode()
+            {
+                const string tagName = "div";
+                const string text = "Please tread carefully!";
+                const string @class = nameof(@class);
+                const string classes = "warn";
+                var e = Element(tagName, Attributes((@class, classes)), text);
+
+                Assert.NotNull(e);
+                Assert.Equal(tagName, e.TagName);
+                Assert.Equal(1, e.Attributes.Count);
+
+                var attr = e.Attributes.Single();
+                Assert.Equal(@class, attr.Name);
+                Assert.Equal(classes, attr.Value);
+
+                Assert.Equal(1, e.ChildNodes.Count);
+                Assert.IsType<HtmlText>(e.FirstChild);
+                Assert.Equal(text, ((HtmlText) e.FirstChild).Value);
+            }
+        }
+
+        public class DocumentFragment
+        {
+            [Fact]
+            public void ReturnsDocumentFragmentInitialized()
+            {
+                var fragment = DocumentFragment();
+                Assert.NotNull(fragment);
+                Assert.Equal(0, fragment.ChildNodes.Count);
+            }
+
+            [Fact]
+            public void WithChildrenReturnsDocumentFragmentInitializedSuch()
+            {
+                var child1 = Text("text");
+                var child2 = Comment("some comment");
+                var child3 = Element("child");
+
+                var fragment = DocumentFragment(child1, child2, child3);
+
+                Assert.NotNull(fragment);
+
+                Assert.Equal(3, fragment.ChildNodes.Count);
+                Assert.Same(child1, fragment.ChildNodes[0]);
+                Assert.Same(child2, fragment.ChildNodes[1]);
+                Assert.Same(child3, fragment.ChildNodes[2]);
+            }
+
+            [Fact]
+            public void WithSequenceOfChildrenReturnsDocumentFragmentInitializedSuch()
+            {
+                var child1 = Text("text");
+                var child2 = Comment("some comment");
+                var child3 = Element("child");
+
+                var children = new HtmlNode[] { child1, child2, child3 };
+                var fragment = DocumentFragment(children.AsEnumerable());
+
+                Assert.NotNull(fragment);
+
+                Assert.Equal(3, fragment.ChildNodes.Count);
+                Assert.Same(child1, fragment.ChildNodes[0]);
+                Assert.Same(child2, fragment.ChildNodes[1]);
+                Assert.Same(child3, fragment.ChildNodes[2]);
+            }
+        }
+
+        public class Template
+        {
+            [Fact]
+            public void ReturnsTemplateNodeInitialized()
+            {
+                var content =
+                    DocumentFragment(
+                        Text("text"),
+                        Comment("some comment"),
+                        Element("child"));
+
+                var template = Template(content);
+
+                Assert.NotNull(template);
+                Assert.Same(content, template.Content);
+            }
+
+            [Fact]
+            public void WithAttributesReturnsTemplateNodeInitializedSuch()
+            {
+                const string name1  = "name1";
+                const string value1 = "value2";
+                const string name2  = "name1";
+                const string value2 = "value2";
+
+                var content =
+                    DocumentFragment(
+                        Text("text"),
+                        Comment("some comment"),
+                        Element("child"));
+
+                var template = Template(Attributes((name1, value1), (name2, value2)), content);
+
+                Assert.NotNull(template);
+
+                Assert.Equal(2, template.Attributes.Count);
+                Attribute.AssertEqual(name1, value1, template.Attributes[0]);
+                Attribute.AssertEqual(name2, value2, template.Attributes[1]);
+
+                Assert.Same(content, template.Content);
+            }
+        }
+
+        public class Document
+        {
+            [Fact]
+            public void ReturnsDocumentNodeInitialized()
+            {
+                HtmlElement html;
+                var document = Document(html = Element("html"));
+
+                Assert.NotNull(document);
+                Assert.Equal(1, document.ChildNodes.Count);
+                Assert.Same(html, document.ChildNodes.Single());
+            }
+
+            [Fact]
+            public void WithChildrenReturnsDocumentNodeInitializedSuch()
+            {
+                var head =
+                    Element("head",
+                        Element("title", "The Document"));
+                var body =
+                    Element("body",
+                        Element("p", "This is a paragraph."));
+
+                var document = Document(head, body);
+
+                Assert.NotNull(document);
+                Assert.Equal(2, document.ChildNodes.Count);
+                Assert.Same(head, document.ChildNodes[0]);
+                Assert.Same(body, document.ChildNodes[1]);
+            }
+        }
+    }
+}
